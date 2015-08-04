f37db70c9dbc81ab9432466e3d07e5ca72cb2337
phagoth <olexandr.averyanov@gmail.com>
2015-08-04 08:26:56 +0300

    done main product controller methods except destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+	validates :name, presence: true
+	validates :price, numericality: { greater_than: 0, allow_nil: true }
+	validates :count, numericality: { greater_than: 0, allow_nil: true }
 end
