1510ae0b5b9f4b457fc31d9a71265d9cbdadd768
Maximddnp <maximapr@yandex.ru>
2015-08-05 12:47:05 +0300

    Add tests for Product controller and create macros for tests

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,30 +2,36 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
 
+	
+
+	it_renders_404_page_when_product_is_not_found :show, :edit, :update, :destroy
+
 	describe "show action" do
 		it "renders show template if a product is found" do
 			product = create(:product)
 			get :show, {id: product.id}
 			response.should render_template('show')
 		end
-
-		it "renders 404 page if a product is not found" do
-			get :show, {id: 0}
-			response.status.should == 404
-		end
 	end	
 
 	describe "create action" do
 		it "redirect to product page if validations pass" do
-			post :create, product: {name: "Product 1", price: '10', count: '1'}
-			response.should redirect_to(product_path(assigns(:@item)))
+			post :create, product: {name: "Product 1", price: 10, count: 1}, id: 33
+			response.should redirect_to(product_path(assigns(:item)))
 		end
 
 		it "renders new page again if validations fail" do
 			post :create, product: {name: "Product 1", price: 0, count: 0}
 			response.should render_template('new')
 		end
-
 	end
+
+		describe "destroy action" do
+			it 'redirects to index action when a product is destroyed succesfully' do
+				product = create(:product)
+				delete :destroy, id: product.id
+				response.should redirect_to(product_path)
+			end
+		end
 
 end
