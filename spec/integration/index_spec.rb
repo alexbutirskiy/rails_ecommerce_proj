453e89ffb391c0ba1cef4e4cfcb5fe65898206e9
Alex Butirskiy <butirskiy@gmail.com>
2015-08-19 11:15:01 +0300

    Specs modified due to new page layouts

--- spec/integration/index_spec.rb
+++ spec/integration/index_spec.rb
@@ -9,40 +9,40 @@ describe 'index page' do
 
   it 'should have all click links' do
     visit main_index_path
-    ['About', 'Store', 'New item', 'Sign in', 'Register', 'Cart'].each do |item|
+    ['Склад', 'Новый продукт', 'Зарегистрироваться', 'Войти', 'Корзина'].each do |item|
       assert page.has_link?(item)
     end
   end
 
-  it 'should do transition from index to about page' do
+  it 'should do transition from index to store page' do
     visit main_index_path
-    find(:xpath, "html/body/header/div/nav/ul[1]/li[1]/a").click
-    assert current_path, '/main/index'
-  end
-
-  it 'should do transition from index to store' do
-    visit main_index_path
-    find(:xpath, "html/body/header/div/nav/ul[1]/li[2]/a").click
+    find(:xpath, "html/body/header/div/ul[1]/li[1]/a").click
     assert current_path, '/products'
   end
 
-  it 'should do transition from index to new item creating menu' do
+  it 'should do transition from index to new item page' do
     visit main_index_path
-    find(:xpath, "html/body/header/div/nav/ul[1]/li[3]/a").click
+    find(:xpath, "html/body/header/div/ul[1]/li[2]/a").click
     assert current_path, '/products/new'
   end
 
   it 'should do transition from index to sign in page' do
     visit main_index_path
-    find(:xpath, "html/body/header/div/nav/ul[2]/li[1]/a").click
-    assert current_path, '/users/sign_in'
+    find(:xpath, "html/body/header/div/ul[2]/li[1]/a").click
+    assert current_path, '/users/sign_up'
   end
   
 
   it 'should do transition from index to sign up page' do
     visit main_index_path
-    find(:xpath, "html/body/header/div/nav/ul[2]/li[2]/a").click
-    assert current_path, '/users/sign_up'
+    find(:xpath, "html/body/header/div/ul[2]/li[2]/a").click
+    assert current_path, '/users/sign_in'
   end
 
+  it 'should do transition from index to sign up page' do
+    visit main_index_path
+    find(:xpath, "html/body/header/div/ul[2]/li[3]/a").click
+    assert current_path, '/cart'
   end
+
+end
