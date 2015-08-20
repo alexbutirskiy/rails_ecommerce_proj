6e93cea40ee10817b7fd49d6abc5a8b61dac8215
Alex Butirskiy <butirskiy@gmail.com>
2015-08-20 20:57:36 +0300

    Default admin has been added to seed
    User: admin@shop.rubysquad.pp.ua
    Password: password

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,12 +32,26 @@ Retailer.create!([
 Product.create!([
   {name: "Молоко детское 1л", price: 19.0, count: 10, category_id: 4, producer_id: 2, retailer_id: 1},
   {name: "Завтрак туриста", price: 17.5, count: 10, category_id: 1, producer_id: 3, retailer_id: 3},
-  {name: "Молоко, 1л.", price: 18.2, count: 10, category_id: 4, producer_id: 2, retailer_id: 2},
+  {name: "Молоко, 1л.",   price: 18.2, count: 10, category_id: 4, producer_id: 2, retailer_id: 2},
   {name: "Соль повареная 1кг", price: 12.0, count: 100, category_id: 1, producer_id: nil, retailer_id: 3},
   {name: "Вода Bonaqua 5л", price: 23.0, count: 15, category_id: 15, producer_id: 1, retailer_id: 1},
-  {name: "Валенки", price: 250.0, count: 3, category_id: 1, producer_id: nil, retailer_id: 1},
-  {name: "Лук репчатый", price: 5.6, count: 24, category_id: 17, producer_id: nil, retailer_id: 2},
-  {name: "Картошка", price: 4.56, count: 120, category_id: 17, producer_id: nil, retailer_id: 2},
-  {name: "Гречка", price: 10.5, count: 15, category_id: 1, producer_id: 1, retailer_id: 1}
+  {name: "Валенки",       price: 250.0, count: 3, category_id: 1, producer_id: nil, retailer_id: 1},
+  {name: "Лук репчатый",  price: 5.6, count: 24, category_id: 17, producer_id: nil, retailer_id: 2},
+  {name: "Картошка",      price: 4.56, count: 120, category_id: 17, producer_id: nil, retailer_id: 2},
+  {name: "Гречка",        price: 11.45, count: 15, category_id: 18, producer_id: 1, retailer_id: 1},
+  {name: "Пшено",         price: 12.5, count: 15, category_id: 18, producer_id: 1, retailer_id: 1},
+  {name: "Рис",           price: 10.5, count: 15, category_id: 18, producer_id: 2, retailer_id: 3},
+  {name: "Прловка 1кг",   price: 10.4, count: 15, category_id: 18, producer_id: 1, retailer_id: 1},
+  {name: "Каша Овсяная",  price: 10.3, count: 15, category_id: 18, producer_id: 3, retailer_id: 2},
+  {name: "Каша Артек",    price: 15.2, count: 15, category_id: 18, producer_id: 3, retailer_id: 2},
+  {name: "Геркулес",      price: 10.1, count: 15, category_id: 18, producer_id: 2, retailer_id: 1}
 ])
 
+user = User.new
+user.email = 'admin@shop.rubysquad.pp.ua'
+user.role = 'admin'
+user.password = 'password'
+user.password_confirmation = 'password'
+user.skip_confirmation_notification!
+user.confirmed_at = Time.now.to_formatted_s(:db)
+user.save!
